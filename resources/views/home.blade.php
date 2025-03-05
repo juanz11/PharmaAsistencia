d7487d3877644c211cac6ff90e2a1fc5defc0ab4
Juan Zamora <uraharazamora@gmail.com>
2025-03-05 14:07:10 -0400

    almuerzo completado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -185,7 +185,7 @@
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                                         </svg>
                                     </div>
-                                    <span class="text-green-600 font-medium">Almuerzo Completado</span>
+                                    <span class="text-green-600 font-medium">Almuerzo</span>
                                 </div>
                                 @endif
                             @elseif($todayAttendance && $todayAttendance->check_out_time)
