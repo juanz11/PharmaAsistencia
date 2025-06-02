b49d4634dc8a29ffdc37b9a181b7c8705a75f594
Juan Zamora <uraharazamora@gmail.com>
2025-06-02 16:00:33 -0400

    excell

--- app/Exports/AttendanceRangeExport.php
+++ app/Exports/AttendanceRangeExport.php
@@ -9,9 +9,11 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithTitle;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use Carbon\Carbon;
 
-class AttendanceRangeExport implements FromCollection, WithHeadings, WithMapping, WithTitle, ShouldAutoSize
+class AttendanceRangeExport implements FromCollection, WithHeadings, WithMapping, WithTitle, ShouldAutoSize, WithStyles
 {
     protected $startDate;
     protected $endDate;
@@ -28,6 +30,14 @@ class AttendanceRangeExport implements FromCollection, WithHeadings, WithMapping
         $data = collect();
 
         foreach ($users as $user) {
+            // Agregar encabezado de empleado
+            $data->push([
+                'user' => $user,
+                'attendance' => null,
+                'date' => null,
+                'is_header' => true
+            ]);
+
             $currentDate = $this->startDate->copy();
             
             // Iterar por cada día en el rango
@@ -47,11 +57,20 @@ class AttendanceRangeExport implements FromCollection, WithHeadings, WithMapping
                 $data->push([
                     'user' => $user,
                     'attendance' => $attendance,
-                    'date' => $currentDate->copy()
+                    'date' => $currentDate->copy(),
+                    'is_header' => false
                 ]);
 
                 $currentDate->addDay();
             }
+
+            // Agregar línea en blanco entre empleados
+            $data->push([
+                'user' => null,
+                'attendance' => null,
+                'date' => null,
+                'is_header' => false
+            ]);
         }
 
         return $data;
@@ -75,11 +94,36 @@ class AttendanceRangeExport implements FromCollection, WithHeadings, WithMapping
         $user = $row['user'];
         $attendance = $row['attendance'];
         $date = $row['date'];
+        $isHeader = $row['is_header'];
+
+        if ($isHeader) {
+            return [
+                $user->name,
+                '',
+                '',
+                '',
+                '',
+                '',
+                ''
+            ];
+        }
+
+        if (!$user) {
+            return [
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+                ''
+            ];
+        }
 
         if (!$attendance) {
             return [
-                $user->name,
-                $date->format('d/m/Y'),
+                '',
+                $date ? $date->format('d/m/Y') : '',
                 'No marcó',
                 'No marcó',
                 'No marcó',
@@ -89,7 +133,7 @@ class AttendanceRangeExport implements FromCollection, WithHeadings, WithMapping
         }
 
         return [
-            $user->name,
+            '',
             $date->format('d/m/Y'),
             $attendance->check_in ? Carbon::parse($attendance->check_in)->format('h:i A') : 'No marcó',
             $attendance->break_start ? Carbon::parse($attendance->break_start)->format('h:i A') : 'No marcó',
@@ -106,8 +150,45 @@ class AttendanceRangeExport implements FromCollection, WithHeadings, WithMapping
         return 'Completo';
     }
 
+    public function styles(Worksheet $sheet)
+    {
+        $lastRow = $sheet->getHighestRow();
+        $currentRow = 1;
+
+        while ($currentRow <= $lastRow) {
+            $value = $sheet->getCell('A' . $currentRow)->getValue();
+            
+            if (!empty($value)) {
+                $sheet->getStyle('A' . $currentRow . ':G' . $currentRow)->applyFromArray([
+                    'font' => [
+                        'bold' => true,
+                        'size' => 14
+                    ],
+                    'fill' => [
+                        'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                        'startColor' => ['rgb' => 'E2E8F0']
+                    ]
+                ]);
+            }
+            $currentRow++;
+        }
+
+        return [
+            1 => [
+                'font' => ['bold' => true],
+                'fill' => [
+                    'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                    'startColor' => ['rgb' => '1A4175']
+                ],
+                'font' => [
+                    'color' => ['rgb' => 'FFFFFF']
+                ]
+            ],
+        ];
+    }
+
     public function title(): string
     {
-        return 'Reporte de Asistencias';
+        return 'Asistencias';
     }
 }
